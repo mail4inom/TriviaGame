00a5e79c411112a5b2a1943ce55a9f4d83437117
Inomzhon Ibragimov <mail4inom@users.noreply.github.com>
2018-03-22 21:41:16 -0400

    innitial changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,86 +30,87 @@ $(document).ready(function () {
             stop();
             conditianal();
             innerHTML();
-            
+
         }
     }
     //stop function:
     function stop() {
         clearInterval(intervalId);
     };
-    //submit
+    //submit button.
     $("#submit").on("click", function () {
         conditianal();
         innerHTML();
+        stop();
     });
     //Conditianal statement that executes after submit button clicked.
-function conditianal(){
-    if ($("#choice3").is(":checked")) {
-        correct++;
-    } else if ($("#choice1, #choice2, #choice4").is(":checked")) {
-        ++incorrect;
-    } else {
-        unanswered++;
-    };
+    function conditianal() {
+        if ($("#choice3").is(":checked")) {
+            correct++;
+        } else if ($("#choice1, #choice2, #choice4").is(":checked")) {
+            ++incorrect;
+        } else {
+            unanswered++;
+        };
 
-    if ($("#choice6").is(":checked")) {
-        correct++;
-    } else if ($("#choice5, #choice7, #choice8").is(":checked")) {
+        if ($("#choice6").is(":checked")) {
+            correct++;
+        } else if ($("#choice5, #choice7, #choice8").is(":checked")) {
 
-        incorrect++;
-    } else {
-        unanswered++;
-    };
+            incorrect++;
+        } else {
+            unanswered++;
+        };
 
-    if ($("#choice9").is(":checked")) {
-        correct++;
-    } else if ($("#choice10, #choice11, #choice12").is(":checked")) {
-        incorrect++;
-    } else {
-        unanswered++;
-    };
+        if ($("#choice9").is(":checked")) {
+            correct++;
+        } else if ($("#choice10, #choice11, #choice12").is(":checked")) {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
 
-    if ($("#choice16").is(":checked")) {
-        correct++;
-    } else if ($("#choice15, #choice13, #choice14").is(":checked")) {
-        incorrect++;
-    } else {
-        unanswered++;
-    };
+        if ($("#choice16").is(":checked")) {
+            correct++;
+        } else if ($("#choice15, #choice13, #choice14").is(":checked")) {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
 
-    if ($("#choice18").is(":checked")) {
-        correct++;
-    } else if ($("#choice17, #choice19, #choice20").is(":checked")) {
-        incorrect++;
-    } else {
-        unanswered++;
-    };
+        if ($("#choice18").is(":checked")) {
+            correct++;
+        } else if ($("#choice17, #choice19, #choice20").is(":checked")) {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
 
-    if ($("#choice23").is(":checked")) {
-        correct++;
-    } else if ($("#choice21, #choice22, #choice24").is(":checked")) {
-        incorrect++;
-    } else {
-        unanswered++;
-    };
+        if ($("#choice23").is(":checked")) {
+            correct++;
+        } else if ($("#choice21, #choice22, #choice24").is(":checked")) {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
 
-    if ($("#choice25").is(":checked")) {
-        correct++;
-    } else if ($("#choice26, #choice27, #choice28").is(":checked")) {
-        incorrect++;
-    } else {
-        unanswered++;
-    };
-}
-function innerHTML(){
-    var html =
-    "<p>All Done!!!</p>" +
-    "<p>Correct Answers: " + correct + "</p>" +
-    "<p>Incorrect Answers: " + incorrect + "</p>" +
-    "<p>Unanswered: " + unanswered + "</p>"
-$("#game").html(html);
-$("#button").show();
-}
+        if ($("#choice25").is(":checked")) {
+            correct++;
+        } else if ($("#choice26, #choice27, #choice28").is(":checked")) {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+    }
+    function innerHTML() {
+        var html =
+            "<p>All Done!!!</p>" +
+            "<p>Correct Answers: " + correct + "</p>" +
+            "<p>Incorrect Answers: " + incorrect + "</p>" +
+            "<p>Unanswered: " + unanswered + "</p>";
+        $("#game").html(html);
+    }
+
 
 });
 
